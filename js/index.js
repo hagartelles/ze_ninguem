e0127cb16500eb179941564c7337e22f411dfdf1
Hagar Telles <hagar.telles@gmail.com>
2023-05-26 06:13:45 -0300

    Updates

--- js/index.js
+++ js/index.js
@@ -2,13 +2,38 @@
 
 const menuMobile = document.getElementById('nav-bar-mobile');
 const btnMenu = document.getElementById('btn-menu');
+const modalForm = document.getElementById('window-modal-form');
 
 menuMobile.addEventListener(' click', animateMenu);
 
 function animateMenu(){
+    
     menuMobile.classList.toggle('mobileMenu');
     btnMenu.classList.toggle('activeBtn');
+    
 }
+
+// modal 
+
+function openForm(){
+    modalForm.classList.add('opened')
+    
+    modalForm.addEventListener("click",(e) => {
+        if (e.target.classList == 'close-btn'|| e.target.id == 'window-modal-form'){
+           modalForm.classList.remove('opened') 
+        }
+    })
+
+    modalForm.addEventListener("scroll",(e) =>{
+        modalForm.classList.remove('opened') 
+    })
+
+    modalForm.addEventListener("scrollend",(e) =>{
+        modalForm.classList.remove('opened') 
+        console.log(1)
+    })
+}
+
 
 
 // SLIDER 
